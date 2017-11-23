4a0ce6c63b1b2627f1da83be769e32593c2467fc
Albert Mashy <albertmashy@mashyindustries.com>
2017-11-24 06:02:43 +1030

    SSL Test

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -3,29 +3,51 @@
 var serveStatic = require('serve-static')
 var slashed = require('slashed')
 var cluster = require('cluster')
+var https = require('https')
+var http = require('http')
 var path = require('path')
+var os = require('os')
 
 if(cluster.isMaster){
-    var cpuCount = require('os').cpus().length
+    let options
+    var cpuCount = os.cpus().length
+
+    var basedir = path.resolve(__dirname, '..')
+    var app = slashed(basedir)
+    app.use(serveStatic(app.get('path:public')))
 
     cluster.on('exit', function (worker) {
         console.log(colors.blue('Worker: ') + worker.id + " died, forking a new one")
         cluster.fork()
         
     })
-    var letsencrypt = require('../ssl/letsencrypt')
+
+    //if(os.platform() == 'linux'){
+        require('../ssl/letsencrypt/letsencrypt')(app, true)
+    //}
+
     //gulp
+
     for (var i = 0; i < cpuCount; i += 1) {
         cluster.fork()
     }
 
 }else{
+    let options
     console.log(colors.blue('Worker: ') + cluster.worker.id + " Running")
-
+    
     var basedir = path.resolve(__dirname, '..')
     var app = slashed(basedir)
     var router = require(path.resolve(basedir, 'routes/baserouter.js'))
     router.use(serveStatic(app.get('path:public')))
     app.use(router)
+
+    http.createServer(app).listen(80)
+    if(os.platform() == 'linux'){
+        options = require('../ssl/letsencrypt/letsencrypt')(app)
+    }
+    if(options){
+        https.createServer(options, app).listen(443)
+    }
 }
 
---
+++ config/ssl.js
@@ -0,0 +1,10 @@
+module.exports = {
+    certDir: "path:ssl/certs",
+    privatekey: "privatekey.pem",
+    publickey: "publickey.pem",
+    cert: "cert.pem",
+    fullchain: "fullchain.pem",
+    chain: "chain.pem",
+    domains: ['example.com'],
+    server: "staging"
+}
